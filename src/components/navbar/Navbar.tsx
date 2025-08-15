21a96214c17d262ec98c0fc0efa063bca73ca13d
taismichely <michely2207@gmail.com>
2025-08-15 18:40:05 -0300

    feat: trocando o flex e alterando cores

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-      <div className="w-full flex justify-center font-bold py-4 text-[#FF5D8F] bg-[#F5F5F5]">
-        <div className="container flex justify-between text-lg">
-          <div className="flex">
-            <Link to='/home' className="text-2xl font-bold">
-              Health <span className="text-[#094E86]">Up</span>
+      <div className="w-full flex justify-center font-bold py-4 text-[#c7a170] bg-white">
+        <div className="container flex flex-wrap items-center justify-between mx-auto">
+          <Link to="/home" className="text-3xl font-bold">
+            Health <span className="text-[#213e60]">Up</span>
+          </Link>
+          <div className="flex gap-4 text-[#213e60] text-xl">
+            <Link to="/categorias" className="hover:text-[#c7a170]">
+              Categorias
             </Link>
           </div>
-          <div className="flex gap-4">
-            <Link to='/categorias' className='hover:underline'>Categorias</Link>
-            <Link to='/cadastrarcategoria' className='hover:underline'>Cadastrar</Link>
-          </div>
+            <button className="bg-[#213e60] hover:bg-[#c7a170] text-white text-xl py-2 px-4 rounded-4xl">
+              <Link to="/cadastrarcategoria" className="">
+                Cadastrar
+              </Link>
+            </button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
