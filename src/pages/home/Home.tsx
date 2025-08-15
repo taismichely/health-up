ef63e1b67130d3dee1ebc1846d27994cdde1238c
taismichely <michely2207@gmail.com>
2025-08-15 18:51:49 -0300

    feat: estilizando o botao na home para categorias

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,7 +11,14 @@ function Home() {
         />
         <div className="absolute inset-0 flex flex-col items-center justify-center text-[#213e60]">
           <h2 className="bg-white text-6xl font-bold mb-4">Remédios </h2>
-          <p className="text-3xl font-bold bg-white">Aqui você encontra tudo o que precisa</p>
+          <p className="text-3xl font-bold bg-white mb-6">
+            Aqui você encontra tudo o que precisa
+          </p>
+          <Link to="/categorias">
+            <button className="text-lg text-white font-bold bg-[#c7a170] hover:bg-[#c7a188] px-4 py-2 rounded-2xl">
+               Veja os nossos produtos!
+            </button>
+          </Link>
         </div>
       </div>
     </>
